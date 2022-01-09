ecd1cb951467473cac8f2872c7c149900b081f23
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-09 21:28:35 +0500

    Update axel.js

--- src/axel.js
+++ src/axel.js
@@ -4,6 +4,7 @@ const Parser = require("./parser.js");
 
 function program (src) {
   let lexer = new Lexer(src);
+  lexer.parsed.forEach(console.log("LEXER " + token));
   let parser = new Parser(src,lexer);
   parser.parse();
 }

45250c7d0c69cbdb0436abf4e5594f857d2ba64b
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-09 20:58:41 +0500

    Create parser.js

---
+++ src/parser.js
@@ -0,0 +1,46 @@
+// parser.js
+
+const keywords = require("./keywords.js");
+const arguments = require("./arguments.js");
+const initializers = require("./initializers.js");
+
+
+
+
+class Parser {
+  constructor (src,lexical) {
+    this.source = src;
+    this.lex = lexical;
+    this.accepted = [].concat(keywords,arguments);
+    
+    this.variables = {}
+    
+  }
+  
+  parse () {
+    let lex = this.lex;
+    let initializer = lex.nextToken();
+    if (!initializers.includes(initializer)) {
+      return false
+    }
+    
+    // log, expression
+    else if (initializer == "log") {
+      console.log(lex.nextToken());
+    }
+    
+    // define, name, =, value
+    else if (initializer == "define") {
+      let key = lex.nextToken();
+      lex.nextToken();
+      let value = lex.nextToken();
+      this.variables[key] = value
+    }
+    
+    else if (initializer == "access" {
+      let name = lex.nextToken();
+      return this.variables[name];
+    }
+    
+  }
+}

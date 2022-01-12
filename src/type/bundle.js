4f13d402d729c693f0262c0784c8e1066f1091a5
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-12 21:43:18 +0500

    Update bundle.js

--- src/type/bundle.js
+++ src/type/bundle.js
@@ -1,1 +1,11 @@
-// 
+// bundle.js
+const Text = require("./text.js");
+const Digit = require("./digit.js");
+const Hash = require("./hash.js");
+
+
+
+
+let exposed = [Text,Digit,Hash];
+
+module.exports = exposed

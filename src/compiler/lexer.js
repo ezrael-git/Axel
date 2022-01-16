315e4e4a22e2e49ef30d080e951b91887bd81ab7
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-16 16:36:37 +0500

    draft

--- src/compiler/lexer.js
+++ src/compiler/lexer.js
@@ -5,19 +5,44 @@ const Type = require("../type/bundle.js");
 class Lexer {
   constructor (source) {
     this.source = source;
-    this.parsed = undefined;
+    this.parsed = source;
 
     this.curPos = -1;
    
   }
 
   lex () {
-    let
+    let parsed = this.parsed;
+    let source = this.source;
+
+    // split initializers
     for (const ini in Initializer) {
-      if (this.source.includes(ini)) {
-        parsed.push(ini);
-      }
+      parsed = parsed.split(ini);
     }
+
+    // check for numbers, and then split them
+    let digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
+
+    parsed.forEach(function (piece) {
+
+      digits.forEach(function (digit) {
+        parsed = piece.split(digit);
+      });
+
+    });
+
+    // now we can check for the parsed digits and convert them
+    parsed.forEach(function (piece) {
+      if (digits.includes(piece)) {
+        let ind = parsed.indexOf(piece);
+        let converted = new Type.Digit(piece);
+        parsed[ind] = converted;
+      };
+    });
+
+    // let's do the same for all the strings
+    
+
   }
   
   nextToken () {

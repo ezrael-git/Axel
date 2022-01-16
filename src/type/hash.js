4de5c865db1bc4ced1a00832eaef52f250ebe22f
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-16 16:06:46 +0500

    Update hash.js

--- src/type/hash.js
+++ src/type/hash.js
@@ -3,23 +3,11 @@
 
 class Hash {
   initialize () {
-    this.keys = [];
-    this.values = [];
+    this.value = {};
   }
   
-  add (k,v) {
-    this.keys.push(k);
-    this.values.push(v);
+  eval () {
+    return this.value;
   }
-  
-  access (k) {
-    let found = undefined;
-    this.keys.forEach(function (key) {
-      if (key == k) {
-        found = this.values[this.keys.index(k)];
-      }
-    };
-    return found;
 
-  }
 }

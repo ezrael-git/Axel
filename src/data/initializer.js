518fb043720f12174e82bb1daa29dd0584625ba7
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-17 20:51:23 +0500

    fix typo

--- src/data/initializer.js
+++ src/data/initializer.js
@@ -5,7 +5,7 @@ module.exports = {
     let parsed = ""
     ctx.args.forEach(arg => parsed += arg.eval());
     console.log(parsed);
-  }
+  },
 
   "@": function (ctx) {
     return `SET ${ctx[0]} ${ctx[2]}`

9fdfa05c98c20e54a65f0c3b932b723a9d844e70
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-18 19:58:17 +0500

    Update baseclass.js

--- src/exception/baseclass.js
+++ src/exception/baseclass.js
@@ -1,4 +1,17 @@
-module.exports = class BaseClass {
-  constructor () {
+module.exports = class Error {
+
+  constructor (pos_start, pos_end, error_name, details) {
+    this.pos_start = pos_start;
+    this.pos_end = pos_end;
+    this.error_name = error_name;
+    this.details = details;
   }
+
+  as_string () {
+    let result  = `${this.error_name}: ${this.details}\n`;
+    result += `File ${self.pos_start.fn}, line ${this.pos_start.ln + 1}`;
+    result += '\n\n' + string_with_arrows(this.pos_start.ftxt, this.pos_start, this.pos_end);
+    return result;
+  }
+
 }

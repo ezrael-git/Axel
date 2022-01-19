8afe8ada1b970b1040e40b3b14d265cff29e6d77
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-19 19:35:46 +0500

    Update lexer.js

--- src/compiler/lexer.js
+++ src/compiler/lexer.js
@@ -18,6 +18,8 @@ class Lexer {
    
   }
 
+  // type manipulation
+
   _rem (arr, item) {
     let n = [];
     arr.forEach(function (a) {
@@ -27,6 +29,19 @@ class Lexer {
     });
     return n;
   };
+
+  _split (str, tokens) {
+    var tempChar = tokens[0]; // We can use the first token as a temporary join character
+    for(var i = 1; i < tokens.length; i++){
+      str = str.split(tokens[i]).join(tempChar);
+    }
+    str = str.split(tempChar);
+    return str;
+  }
+
+  // processing
+
+
 
   process_str (item, starting, blocking=this.blocking) {
     let saved = "";
@@ -61,6 +76,8 @@ class Lexer {
     });
     return saved;
   }
+
+  // lexing
 
   lex () {
 

c887b242869cf9cbd97e2b6a709c0cd937f75c53
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-20 21:17:15 +0500

    Update bundle.js

--- src/type/bundle.js
+++ src/type/bundle.js
@@ -5,7 +5,9 @@ module.exports = {
   Hash: require("./hash.js"),
   List: require("./list.js"),
   Text: require("./text.js"),
-  Digit: require("./digit.js")
+  Digit: require("./digit.js"),
+
+  Paren: require("./parentheses.js")
 
 }
 

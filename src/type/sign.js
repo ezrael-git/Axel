11d286e51bf6dd79dfdf951f309b8079f018874c
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-20 23:09:34 +0500

    Update sign.js

--- src/type/sign.js
+++ src/type/sign.js
@@ -1,41 +1,43 @@
 module.exports = class Sign {
+  static signs = {}
+
+  static Div = class Div {
+    static value = "/"
+    static eval () {
+      return value
+    }
+  }
+
+  static Mul = class Mul {
+    static value = "*"
+    static eval () {
+      return value
+    }
+  }
+
+  static Add = class Add {
+    static value = "+"
+    static eval () {
+      return value
+    }
+  }
+
+  static Sub = class Sub {
+    static value = "-"
+    static eval () {
+      return value
+    }
+  }
+
   constructor (value) {
 
     this.value = value;
-    static signs = {}
 
-    static Div = class Div {
-      static value = "/"
-      static eval () {
-        return value
-      }
-    }
-
-    static Mul = class Mul {
-      static value = "*"
-      static eval () {
-        return value
-      }
-    }
-
-    static Add = class Add {
-      static value = "+"
-      static eval () {
-        return value
-      }
-    }
-
-    static Sub = class Sub {
-      static value = "-"
-      static eval () {
-        return value
-      }
-    }
 
   }
 
   static signs () {
-    return this.signs;
+    return Sign.signs;
   }
 
   eval () {

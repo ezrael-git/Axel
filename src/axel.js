ac9a03d1bc44f3fe44d1ecd0c35a300e0740f9b5
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-22 18:19:25 +0500

    Update axel.js

--- src/axel.js
+++ src/axel.js
@@ -1,21 +1,23 @@
 const Lexer = require("./compiler/lexer.js");
 const Parser = require("./compiler/parser.js");
+const Emitter = require("./compiler/emitter.js");
 
 class Axel {
 
   constructor () {
     this.parser = new Parser();
+    this.emitter = new Emitter();
   }
 
   program (src) {
     let strp = src.split(';');
     let log = [];
-    let parser = this.parser;
-    strp.forEach(function (line) {
+    for (let line of strp) {
       let lex = new Lexer(line);
       let r = parser.parse(lex);
       log.push(r);
-    });
+    }
+    emitter.add(parser.emitted);
     return log;
   }
 

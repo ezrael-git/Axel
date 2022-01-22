1d1f9b25db6aee51dbb7f12edd40349a9e799a10
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-22 17:04:32 +0500

    Create emitter.js

---
+++ src/compiler/emitter.js
@@ -0,0 +1,12 @@
+// executor / collector / emitter
+
+module.exports = class Emitter {
+  constructor (in_it) {
+    this.parsed = in_it;
+  }
+
+  eval () {
+    eval(this.parsed);
+    console.log("Axel main.js at 0");
+  }
+}

d4702bacc16046ba19c9129339fc9f8537efdf4d
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-31 19:13:48 +0500

    Update stdblib.js

--- src/standard/stdblib.js
+++ src/standard/stdblib.js
@@ -1,3 +1,21 @@
-fn log expr
+imm Lexer = require("./lexer.js")
+imm Parser = require("./parser.js")
+
+fn log (expr)
   console.log(expr)
+end
+
+fn include (path)
+  return require(path)
+end
+
+fn compile (statements)
+  statements = statements.trim().split('\n')
+  for line in statements
+    def lexer = new Lexer(line);
+    def lex = lexer.lex();
+    this.parser.parse(lex,line);
+  end
+  this.emitter.add(this.parser.emitted);
+  this.emitter.eval();
 end

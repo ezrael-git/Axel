a10f2a2a5de9cf14d05e85b2cd42addbce813906
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-02-04 20:30:23 +0500

    Update preprocesser.js

--- src/compiler/preprocesser.js
+++ src/compiler/preprocesser.js
@@ -29,6 +29,120 @@ module.exports = class Preprocessor {
     return a;
   }
 
+  identify_macros (stats) {
+    /*
+    Identify the macros in a list of statements.
+    */
+    let elem = 0;
+    let macros = [];
+    let flag = false;
+    for (let line of stats) {
+      elem += 1;
+      if (line.includes("+++")) {
+        if (flag == false) {
+          flag = true;
+        } else {
+          flag = false;
+        }
+      }
+      if (flag == true) {
+        macros.push(line);
+      }
+    }
+    let prepr = [];
+    for (let macro of macros) {
+      prepr.push(macro.replaceAll("+++", ""));
+    }
+    return prepr;
+  }
+
+  process_macros (macros) {
+    /*
+    Process and execute macros from a list of macros.
+    */
+    let newlined = macros.join('\n');
+    eval(newlined);
+  }
+
+  cleanse_macros (stats) {
+    /*
+    Remove all macros from a list of statements.
+    */
+    let compiled_stats = [];
+    let flag = true;
+    for (let stat of stats) {
+      if (stats.includes("+++")) {
+        if (flag == true) {
+          flag = false;
+        } else {
+          flag = true;
+        }
+      }
+      if (flag == true) {
+        compiled_stats.push(stat);
+      }
+    }
+    return compiled_stats;
+  }
+
+  get_functions (stats) {
+    /*
+    Get names of all functions in a list of statements of Axel code.
+    */
+    let functions = [];
+    for (line of stats) {
+      if (line.startsWith("fn ")) {
+        line = line.replace("fn ", "");
+        let name = "";
+        for (char of line) {
+          if (char == " " || char == "(") {
+            break;
+          }
+          name += char;
+        }
+        functions.push(name);
+      }
+    }
+    return functions;
+  }
+
+  get_classes (stats) {
+    /*
+    Get names of all classes in a list of statements of Axel code.
+    */
+    let classes = [];
+    for (let line of stats) {
+      if (line.startsWith("cls ")) {
+        line = line.replace("cls ", "");
+        let name = "";
+        for (let char of line) {
+          if (char == " " || char == "(") {
+            break;
+          }
+          name += char;
+        }
+        classes.push(name);
+      }
+    }
+    return classes;
+  }
+
+
+  do_some_replacing (stdlib, code) {
+    /*
+    Replace the placeholding terms in the stdlib to contain information.
+    */
+    let n = [];
+    let functions = this.get_functions(code);
+    let classes = this.get_classes(code);
+    for (line of stdblib) {
+      line = line.replaceAll("__functions__", functions);
+      line = line.replacrAll("__classes__", classes);
+      n.push(line);
+    }
+    return n;
+  }
+
   process (code) {
     return this.literal_replace(code);
   }

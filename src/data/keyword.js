ba2de817c3e5737784103eaca61492871fd8216c
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-02-19 15:59:01 +0500

    Update keyword.js

--- src/data/keyword.js
+++ src/data/keyword.js
@@ -1,10 +1,20 @@
 let declarating = ["imm", "def", "fn"];
 let operators = ["+", "-", "/", "*", "="];
 
+function merge (one,two) {
+  let three = [];
+  for (let a of one) {
+    three.push(a)
+  }
+  for (let b of two) {
+    three.push(b)
+  }
+  return three;
+}
 let bundle = {
   declarating: declarating,
   operators: operators,
-  all: declarating + operators + ["return"]
+  all: merge(declarating,operators).push("return")
 
 }
 

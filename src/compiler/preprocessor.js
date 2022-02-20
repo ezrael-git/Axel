79854cea4371d1dcdd829907ed81c32fe0a3b702
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-02-20 17:18:25 +0500

    Update preprocessor.js

--- src/compiler/preprocessor.js
+++ src/compiler/preprocessor.js
@@ -1,7 +1,10 @@
+const Scanner = require("./scanner.js");
+
 module.exports = class Preprocessor {
   constructor () {
     this.variables = [];
     this.imports = [];
+    this.scanner = new Scanner();
   }
 
 
@@ -15,77 +18,6 @@ module.exports = class Preprocessor {
     return fmtd;
   }
 
-  scan_variables (stats) {
-    let objs = {};
-    for (let stat of stats) {
-      if (stat.startsWith("def") || stat.startsWith("imm")) {
-        let name = stat.split(' ')[1];
-        let value = stat.replace("def " + name, "").replace("imm " + name, "").replace(" = ", "");
-        objs[name] = value;
-      }
-    }
-    this.variables = objs;
-    return objs;
-  }
-
-  scan_imports (stats) {
-    let objs = {}
-    for (let stat of stats) {
-      if (stat.startsWith("import")) {
-        let filename = stat.split(' ')[1];
-        let pkgname = stat.split('as')[1];
-        objs[filename] = pkgname;
-      }
-    }
-    this.imports = objs;
-    return objs;
-  }
-
-  scan_blank_references (stats) {
-    let objs = {};
-    let line = -1;
-    for (let stat of stats) {
-      let stat_copy = stat
-      line += 1;
-      if (stat.includes("@=>")) {
-        let iterated = "";
-        for (let char of stat) {
-          iterated += char;
-          if (iterated.includes("@=>")) {
-            stat = stat.replace(iterated, "");
-            break;
-          }
-        }
-        let varName = "";
-        for (let char of stat) {
-          if (char == " " || char == ")") {
-            break;
-          }
-          varName += char;
-        }
-        objs[varName] = stat_copy;
-      }
-    }
-    
-    return objs;
-  }
-
-  scan_unholy_calls (stats) {
-    let functions = [];
-    let line = 0;
-    for (let stat of stats) {
-      line += 1;
-      if (stat.startsWith("fn ") || stat.includes("meth")) {
-        functions.push(stat.split(' ')[1].replace("(", ""));
-      }
-
-      for (let func of functions) {
-        if (stat.includes(func + "(") && !stat.includes("new ")) {
-          throw SyntaxError(`Functions should be called with the call: keyword\nIn line ${line}: ${stat}`);
-        }
-      }
-    }
-  }
 
   cleanse_calls (stats) {
     let man = [];
@@ -115,9 +47,9 @@ module.exports = class Preprocessor {
 
   hoist (stats) {
     let manipulated = [];
-    let vars = this.scan_variables(stats);
-    let imports = this.scan_imports(stats);
-    let blank_refs = this.scan_blank_references(stats);
+    let vars = this.scanner.scan_variables(stats);
+    let imports = this.scanner.scan_imports(stats);
+    let blank_refs = this.scanner.scan_blank_references(stats);
     for (let filename in imports) {
       let pkgname = imports[filename];
       manipulated.push(`const ${pkgname} = require('${filename}')`)
@@ -139,17 +71,7 @@ module.exports = class Preprocessor {
     return manipulated;
   }
 
-
-
-  process (code) {
-    /*
-    Interface
-    */
-    let fm = code;
-    fm = this.remove_comments(fm);
-    fm = this.hoist(fm);
-    this.scan_unholy_calls(fm);
-
+  handle_calls (fm) {
     let line = -1;
     for (let stat of fm) {
       line += 1;
@@ -174,6 +96,21 @@ module.exports = class Preprocessor {
         fm = this.cleanse_calls(fm);
       }
     }
+
+    return fm;
+  }
+
+
+
+  process (code) {
+    /*
+    Interface
+    */
+    let fm = code;
+    fm = this.remove_comments(fm);
+    fm = this.hoist(fm);
+    this.scanner.scan_unholy_calls(fm);
+    fm = this.handle_calls(fm);
 
     line = -1;
     for (let stat of fm) {

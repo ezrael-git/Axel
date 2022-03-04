0f7ed2dae981af98083cc5bedd71a99ef554da5b
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-03-04 17:49:05 +0500

    Update lexer.js

--- src/compiler/lexer.js
+++ src/compiler/lexer.js
@@ -2,14 +2,15 @@
 const Scanner = require("./scanner.js");
 let TT_LPAREN = "("
 let TT_RPAREN = ")"
-let TT_FN = "fn"
-let TT_DEF = "def"
-let TT_IMM = "imm"
 let TT_EQ = "="
 let TT_PLUS = "+"
 let TT_MINUS = "-"
 let TT_MULTIPLY = "*"
 let TT_DIVIDE = "/"
+
+let TT_FN = "fn"
+let TT_DEF = "def"
+let TT_IMM = "imm"
 
 
 
@@ -46,13 +47,12 @@ class Lexer {
 
     let it = "";
     let pos = -1;
-    let inString = false;
-    let changingString = "";
     while (pos != source.length + 1) {
       pos += 1;
       let char = source[pos];
       it += char;
 
+      // handle parentheses
       if (it.endsWith(TT_LPAREN) && sc.inQuotes(source,pos) == false || it.endsWith(TT_RPAREN) && sc.inQuotes(source,pos) == false) {
         if (tt.endsWith(TT_LPAREN)) {
           add("LPAREN", TT_LPAREN);
@@ -62,10 +62,37 @@ class Lexer {
       }
       // beginning of string
       else if (it.endsWith("'") && sc.inQuotes(source,pos) == true || it.endsWith('"') && sc.inQuotes(source,pos) == true) {
-        inString = true;
+        // get the exact quote used
         changingString = it[it.length - 1]
-      
+        // get full string
+        let fullString = sc.getUntil(source,pos,changingString);
+        add("STRING", fullString.string);
+        pos = fullString.curPos;
       }
+      // addition operator
+      else if (it.endsWith(TT_PLUS)) {
+        add("PLUS", TT_PLUS);
+      }
+      else if (it.endsWith(TT_MINUS)) {
+        add("MINUS", TT_MINUS);
+      }
+      else if (it.endsWith(TT_MULTIPLY)) {
+        add("MULTIPLY", TT_MULTIPLY);
+      }
+      else if (it.endsWith(TT_DIVIDE)) {
+        add("DIVIDE", TT_DIVIDE);
+      }
+      else if (it.endsWith(TT_EQ)) {
+        add("EQUALITY", TT_EQ);
+      }
+      else if (it.endsWith(TT_FN) && this.back(pos) == " " && this.peek(pos) != " ") {
+        add("FUNCTION", TT_FN);
+        let identifier = sc.getUntil(source,pos+2," ");
+        add("IDENTIFIER", identifier.string);
+        pos = identifier.curPos;
+        let args = source.replace(
+      }
+      
 
     }
 

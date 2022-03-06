b9149196fd315c4911cbbfce12eda3f440ba99bc
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-03-06 19:44:39 +0500

    Update parser.js

--- src/compiler/parser.js
+++ src/compiler/parser.js
@@ -27,12 +27,22 @@ module.exports = class Parser {
     return n;
   }
 
+  current () {
+    return this.tokens[this.token_iterated];
+  }
+
   peek (tokens=1) {
     return this.tokens.slice(this.token_iterated,this.token_iterated + tokens);
   }
 
   lookBack (tokens=1) {
     return this.tokens.slice(this.token_iterated,this.token_iterated - tokens);
+  }
+
+  handleType (token) {
+    if (token.type == "STRING") {
+      return 
+    }
   }
 
   
@@ -45,9 +55,41 @@ module.exports = class Parser {
     this.tokens = tks;
     this.token_iterated = -1;
 
-    let node = [];
+    let node_tree = [];
     while (this.peek() != undefined) {
-      
+      let token = this.next();
+      let type = token.type;
+      if (type == "PLUS") {
+        let lhs = this.lookBack();
+        let rhs = this.peek();
+        let node = new Node.BinaryOperatorNode(lhs,rhs,"+");
+        node_tree.push(node);
+      }
+      else if (type == "MINUS") {
+        let lhs = this.lookBack();
+        let rhs = this.peek();
+        let node = new Node.BinaryOperatorNode(lhs,rhs,"-");
+        node_tree.push(node);
+      }
+      else if (type == "MULTIPLY") {
+        let lhs = this.lookBack();
+        let rhs = this.peek();
+        let node = new Node.BinaryOperatorNode(lhs,rhs,"*");
+        node_tree.push(node);
+      }
+      else if (type == "DIVIDE") {
+        let lhs = this.lookBack();
+        let rhs = this.peek();
+        let node = new Node.BinaryOperatorNode(lhs,rhs,"/");
+        node_tree.push(node);
+      }
+      else if (type == "DEFINE") {
+        if (this.next().type != "EQUALITY") {
+          throw new Error(`Expected TokenType to be EQUALITY, got ${this.current().type} instead`);
+        }
+        let value_token = this.next();
+        
+      }
     }
   }
 

5584c305cef465cf00eed9fe461b6d990bc9b15a
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-03-06 18:07:56 +0500

    Update nodes.js

--- src/compiler/nodes.js
+++ src/compiler/nodes.js
@@ -3,53 +3,84 @@
 
 
 class VarAssignNode {
-  constructor (name_node, value_node, line_node) {
-    this.type = "declaration";
-    this.name_node = name_node;
-    this.value_node = value_node;
-    this.line_node = line_node;
+  constructor (name, value_type, value, line, start, end) {
+    this.type = "DeclarationExpression";
+    this.body = {
+      name:name,
+      line:line,
+      start:start,
+      end:end,
+      value_type:value_type,
+      value:value
+    }
+  }
+}
+
+class VarAccessNode {
+  constructor (name, line, start, end) {
+    this.type = "AccessExpression"
+    this.body = {
+      line:line,
+      start:start,
+      end:end
+    }
   }
 }
 
 class FuncAssignNode {
-  constructor (name_node, arg_node, body_node, auto_return_node, line_node) {
-    this.type = "declaration";
-    this.name_node = name_node;
-    this.arg_node = arg_node;
-    this.body_node = body_node;
-    this.auto_return_node = auto_return_node;
-    this.line_node = line_node;
-  }
-}
-
-class TextNode {
-  constructor (value_node, line_node) {
-    this.type = "text";
-    this.value_node = value_node;
-    this.line_node = line_node;
-  }
-
-  toString () {
-    return this.value_node;
-  }
-}
-
-class IntegerNode {
-  constructor (value_node, line_node) {
-    this.type = "integer";
-    this.value_node = value_node;
-    this.line_node = line_node;
-  }
-
-  toString () {
-    return this.value_node;
+  constructor (name, line, start, end, args, body) {
+    this.type = "DeclarationExpression";
+    this.body = body;
+    this.body["args"] = args;
+    this.body["start"] = start;
+    this.body["end"] = end;
+    this.body["line"] = line;
   }
 }
 
 class CallNode {
-  constructor (node, arg_nodes, line_node) {
-    this.node_to_call = node;
-    this.arg_nodes = arg_nodes;
-    this.line_node = line_node;
+  constructor (node_to_call, args, line, start, end) {
+    this.type = "CallExpression";
+    this.body = {
+      callee:node_to_call,
+      args:args,
+      line:line,
+      start:start,
+      end:end
+    }
   }
 }
+
+class TextNode {
+  constructor (value, line, start, end) {
+    this.type = "TextExpression";
+    this.body = {
+      value:value,
+      line:line,
+      start:start,
+      end:end
+    }
+  }
+
+  toString () {
+    return this.body.value;
+  }
+}
+
+class IntegerNode {
+  constructor (value, line, start, end) {
+    this.type = "IntegerExpression";
+    this.body = {
+      value:value,
+      line:line,
+      start:start,
+      end:end
+    }
+  }
+
+  toString () {
+    return this.body.value;
+  }
+}
+
+

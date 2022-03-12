fc34318a5d09dc0478b7c41e34073ad1474b690c
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-03-12 18:50:52 +0500

    Update walker.js

--- src/compiler/walker.js
+++ src/compiler/walker.js
@@ -58,7 +58,7 @@ module.exports = class Walker {
       let name = node.body.name;
       let getMethods = (obj) => Object.getOwnPropertyNames(obj).filter(item => typeof obj[item] === 'function')
       console.log("VALUE " + JSON.stringify(node.body.value));
-      let value = node.body.value.run(variables,walker);
+      let value = node.body.value.run(this.variables,new Walker());
       let mutable = node.body.mutable;
       if (this.variables[name] == undefined) {
         this.variables[name] = value;

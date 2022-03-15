d4ab243cfee6d508659a3185f98d14502b2cdac1
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-03-15 19:30:50 +0500

    Update literals.js

--- src/compiler/literals.js
+++ src/compiler/literals.js
@@ -39,7 +39,40 @@ class IntegerLiteral {
   }
 }
 
+class TrueLiteral {
+  constructor () {
+    this.value = "true";
+  }
+
+  run () {
+    return this.value;
+  }
+}
+class FalseLiteral {
+  constructor () {
+    this.value = "false";
+  }
+
+  run () {
+    return this.value;
+  }
+}
+
+class NilLiteral {
+  constructor () {
+    this.value = "nil";
+  }
+
+  run () {
+    return this.value;
+  }
+}
+
+
 module.exports = {
   TextLiteral:TextLiteral,
-  IntegerLiteral:IntegerLiteral
+  IntegerLiteral:IntegerLiteral,
+  TrueLiteral:TrueLiteral,
+  FalseLiteral:FalseLiteral,
+  NilLiteral:NilLiteral
 }

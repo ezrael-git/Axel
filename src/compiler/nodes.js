08dfe7aee5f88897f79fe63d779f55921b1ba095
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-03-17 17:58:13 +0500

    Update nodes.js

--- src/compiler/nodes.js
+++ src/compiler/nodes.js
@@ -217,6 +217,10 @@ class BinaryOperatorNode {
       return lhs / rhs
     } else if (this.body.op == "*") {
       return lhs * rhs
+    } else if (this.body.op == "==") {
+      return lhs === rhs
+    } else if (this.body.op == "!=") {
+      return lhs !== rhs
     }
   }
 }

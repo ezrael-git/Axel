52695e321d320899d7dc886bf9a818d0c818d310
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-03-25 14:49:55 +0500

    Update nodes.js

--- src/compiler/nodes.js
+++ src/compiler/nodes.js
@@ -282,12 +282,18 @@ class IfNode {
   runCondition (v,w) {
     w.variables = v; // pass global variables to the ifNode interpreter
     let conditionResult = w.interpretNode(this.body.condition,this.body.condition.constructor.name);
+    console.log("first");
+    console.log(JSON.stringify(conditionResult) + " " + conditionResult.constructor.name);
     if (conditionResult.run != undefined && !conditionResult.constructor.name.includes("Literal")) {
       conditionResult = conditionResult.run();
     }
+    console.log("second")
+    console.log(JSON.stringify(conditionResult) + " " + conditionResult.constructor.name);
     if (conditionResult.constructor.name.includes("Literal")) {
       conditionResult = String(conditionResult.to_b());
     }
+    console.log("third")
+    console.log(JSON.stringify(conditionResult) + " " + conditionResult.constructor.name);
     return conditionResult;
   }
 

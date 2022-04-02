cdd5aa34fd7337f5a4e11974c7be393b7569b1e2
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-04-02 21:52:08 +0500

    Update axel.js

--- src/axel.js
+++ src/axel.js
@@ -19,19 +19,43 @@ class Axel {
     */
   }
 
+  collectMicros (stats) {
+    let m = [];
+    for (let stat of stats) {
+      if (stat.startsWith("#")) {
+        m.push(stat);
+      }
+    }
+    return m;
+  }
+
 
 
   program (statements) {
     /*
     One-for-all interface to execute Axel code. This function acts as a middleman between the code and the compiler, passing the statements into the compiler and executing it in the end.
     */
+    // default settings
+    // can be edited through micros
+    let preprocessor = true;
+
     statements = statements.replaceAll(';', '\n').trim().split('\n');
-    statements = this.preprocessor.process(statements);
+    // execute micros
+    let micros = this.collectMicros(statements);
+    for (let m of micros) {
+      eval(m);
+    }
+
+    // compile the statements
+    // optional compiling stages are under if statements and can be edited through micros
+    if (preprocessor == true) {
+      statements = this.preprocessor.process(statements);
+    }
 
     let tokens = this.lexer.process(statements);
     let ast = this.parser.parseProgram(tokens);
     let o = this.interpreter.walk(ast);
-    return "return " + o;
+    return o
   }
 
 }
